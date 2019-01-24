2dea9f6c03ee8ada0ba15049e4114edeb0c7c997
Sean Bills <seanmichaelbills@gmail.com>
2019-01-24 14:11:46 -0500

    implemented method of calculating the percent change between last iteration of checks and current iteration (will get used later for "recent history" implementation); implemented method to update the manual watchlist specified by the user; need to develop way of processing a user's input and deciding how to behave from that

--- utils.py
+++ utils.py
@@ -6,12 +6,13 @@ from yahoo_fin import stock_info as si
 
 def updateStockValuations(tickers, currentValues):
     while True:
-        for ticker in tickers[0:5]:
+        for ticker in tickers:
             try:
                 ## just want the live price of the stock (since we aren't including
                 ##      any predictive features in yet based on past performance)
                 value = si.get_live_price(ticker)
                 if value is not None:
+                    priceChange = percentChanged(currentValues[ticker], value)
                     currentValues[ticker] = value
                     print (ticker + ": " + str(currentValues[ticker]))
             except ValueError:
@@ -23,3 +24,16 @@ def updateCurrentTotalValuation(currentValues, totalOwned, currentTotalValuation
         for owned in list(totalOwned.keys()):
             currentTotalValuation += (currentValues[owned] * totalOwned[owned])
             print("Total Valuation: " + str(currentTotalValuation))
+
+def percentChanged(oldValue, newValue):
+    change = (oldValue - newValue) / 100
+    return change
+
+def manualWatchlistUpdate(watchlist):
+    while True:
+        userSpecification = input()
+        processUserInput(userSpecification)
+
+def processUserInput(input):
+    args = input.split(" ")
+    
