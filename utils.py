035a0e5e889f0a55c7fa88fb98e1faefba5e908d
Sean Bills <seanmichaelbills@gmail.com>
2019-01-24 15:00:10 -0500

    added methods for processing command line input and adding/updating/removing watch requests based upon pertinent information provided to command line input

--- utils.py
+++ utils.py
@@ -29,11 +29,51 @@ def percentChanged(oldValue, newValue):
     change = (oldValue - newValue) / 100
     return change
 
-def manualWatchlistUpdate(watchlist):
+def processManualInput(watchlist):
     while True:
         userSpecification = input()
-        processUserInput(userSpecification)
+        response = processUserInput(userSpecification, watchlist)
+        if response is not None:
+            print(response)
+        else:
+            print("Sorry, your request could not be processed. Please make"
+                + " sure your inputs match the expected form.")
 
-def processUserInput(input):
+
+def processUserInput(input, watchlist, totalOwned):
     args = input.split(" ")
-    
+    if args[0] == "-b" or args[0] == "-B":
+        processPurchaseRequest(args[1:])
+    if args[0] == "-s" or args[0] == "-S":
+        processSellRequest(args[1:])
+    if args[0] == "-w" or args[0] == "-W":
+        if len(args) == 6:
+            return processWatchRequest(args[1], args[2], args[3], args[4], args[5], watchlist)
+        else:
+            return None
+    if args[0] == "-rw" or args[0] == "-RW" or args[0] == "-rW" or args[0] == "-Rw":
+        if len(args) == 2:
+            return processRemoveWatch(args[1], watchlist, totalOwned)
+        else:
+            return None
+    return None
+
+def processWatchRequest(abbrev, lowSell, highSell, lowBuy, highBuy, watchlist):
+    order = WatchOrder(abbrev, lowSell, highSell, lowBuy, highBuy)
+    if order not in watchlist:
+        watchlist.append(order)
+        return "Successfully added your watch request!"
+    else:
+        for x in xrange(len(watchlist)):
+            if watchlist[x] == order:
+                watchlist[x] = order
+        return "This stock is already in your watchlist! Updated your buy/sell thresholds with the new values."
+
+def processRemoveWatch(stock, watchlist, totalOwned):
+    if totalOwned[stock] == 0:
+        for x in xrange(len(watchlist)):
+            if watchlist[x].getAbbrev() == stock.upper():
+                del watchlist[x]
+                return "Successfully removed watchlist for " + stock.upper()
+    else:
+        return "Failure! Cannot remove a watch for a stock you own."
